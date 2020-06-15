b07d56d9ac01dd5b2824ff6ec900ae2a4b28b7ce
L-Horatio <483583753@qq.com>
2020-06-15 23:26:40 +0800

    更新：收货地址--增加--控制器层

--- src/main/java/cn/tedu/store/controller/DistrictController.java
+++ src/main/java/cn/tedu/store/controller/DistrictController.java
@@ -4,6 +4,7 @@ import cn.tedu.store.entity.District;
 import cn.tedu.store.service.IDistrictService;
 import cn.tedu.store.util.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,8 @@ public class DistrictController extends BaseController{
     @Autowired
     private IDistrictService districtService;
 
-    @RequestMapping("/list")
-    public ResponseResult<List<District>> getListByParent(@RequestParam("parent") String parent) {
+    @RequestMapping("/list/{parent}")
+    public ResponseResult<List<District>> getListByParent(@PathVariable("parent") String parent) {
         List<District> list = districtService.getListByParent(parent);
         return new ResponseResult<List<District>>(SUCCESS, list);
     }
